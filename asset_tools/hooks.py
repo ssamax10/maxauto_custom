86a50683925ae4b0a9162176c4091155e357a7d1
Shridhar <shridhar.sa@maxautocables.com>
2026-01-21 00:25:27 +0530

    updated the hooks file

--- asset_tools/hooks.py
+++ asset_tools/hooks.py
@@ -4,3 +4,11 @@ app_publisher = "Max Auto Cables Pvt Ltd"
 app_description = "Asset QR label printing and utilities"
 app_email = "info@maxautocables.com"
 app_license = "MIT"
+# Pages
+page_list = [
+	{
+		"page_name": "asset-label-sheet",
+		"title": "Asset Label Sheet",
+		"icon": "fa fa-barcode"
+	}
+]
---
+++ asset_tools/page/asset_label_sheet/asset_label_sheet.py
@@ -0,0 +1,5 @@
+import frappe
+from frappe import _
+
+def get_context(context):
+	context.no_cache = 1
